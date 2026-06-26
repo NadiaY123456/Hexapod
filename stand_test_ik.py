7f7599a9af8eba72ccecb00799d0abbd0bed47e8
Justin <justinyang467@gmail.com>
2026-06-25 18:16:43 -0700

    raise rest pose and walking lift

--- stand_test_ik.py
+++ stand_test_ik.py
@@ -111,8 +111,8 @@ KNEE_STAND_EXTRA_DURING_LIFT = 20.0
 
 # Final pose from commit 70a7ad9, used as a second-phase drop after the
 # restored 229222f stand-up pose.
-DROP_TO_FOOT_ANGLE = 5.0
-DROP_TO_KNEE_ANGLE = 72.0
+DROP_TO_FOOT_ANGLE = 12.0
+DROP_TO_KNEE_ANGLE = 76.0
 DROP_TO_POSE = [
     DROP_TO_FOOT_ANGLE - NEUTRALS["leg1"]["foot"],
     DROP_TO_KNEE_ANGLE - NEUTRALS["leg1"]["knee"],
@@ -130,8 +130,8 @@ WALK_CYCLES = 4
 TRIPOD_A = ("leg1", "leg3", "leg5")
 TRIPOD_B = ("leg2", "leg4", "leg6")
 
-WALK_LIFT_FOOT_DELTA = 12.0
-WALK_LIFT_KNEE_DELTA = -10.0
+WALK_LIFT_FOOT_DELTA = 18.0
+WALK_LIFT_KNEE_DELTA = -14.0
 WALK_HIP_SWING_DEG = 12.0
 WALK_LIFT_STEPS = 25
 WALK_HIP_STEPS = 30
