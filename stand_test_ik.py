2234ad6c439f0006938618d72ad4cb9944ded54f
Justin <justinyang467@gmail.com>
2026-06-30 17:08:20 -0700

    move feet outward from hard stop

--- stand_test_ik.py
+++ stand_test_ik.py
@@ -113,7 +113,7 @@ FOOT_COMMAND_SHIFT = 90.0
 
 # Final pose from commit 70a7ad9, used as a second-phase drop after the
 # restored 229222f stand-up pose.
-DROP_TO_FOOT_ANGLE = 4.0
+DROP_TO_FOOT_ANGLE = 10.0
 DROP_TO_KNEE_ANGLE = 88.0
 DROP_TO_POSE = [
     DROP_TO_FOOT_ANGLE - NEUTRALS["leg1"]["foot"],
