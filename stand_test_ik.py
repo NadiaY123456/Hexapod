1dd0d990e1102eecf4624eb4ca43a08a735d4332
Justin <justinyang467@gmail.com>
2026-07-01 17:41:32 -0700

    increase backward veer correction

--- stand_test_ik.py
+++ stand_test_ik.py
@@ -152,12 +152,12 @@ HIP_SWING_SCALE = {
     "leg6": 1.02,
 }
 BACKWARD_HIP_SWING_SCALE = {
-    "leg1": 1.02,
-    "leg2": 0.98,
-    "leg3": 1.02,
-    "leg4": 0.98,
-    "leg5": 1.02,
-    "leg6": 0.98,
+    "leg1": 0.96,
+    "leg2": 1.04,
+    "leg3": 0.96,
+    "leg4": 1.04,
+    "leg5": 0.96,
+    "leg6": 1.04,
 }
 STANCE_HIP_SCALE = {
     "leg1": 1.0,
