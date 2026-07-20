a35e0d53662c6031ed262e400af9c091f69e74d6
NadiaY123456 <62516100+NadiaY123456@users.noreply.github.com>
2026-07-20 16:51:15 -0700

    Refactor LED test to run in a loop with interrupt handling

--- led_test.py
+++ led_test.py
@@ -3,6 +3,12 @@ from time import sleep
 
 led = LED(27)  # GPIO27, physical pin 13
 
-led.on()
-sleep(10)
-led.off()
+try:
+    while True:
+        led.on()
+        sleep(0.5)
+        led.off()
+        sleep(0.5)
+
+except KeyboardInterrupt:
+    led.off()
